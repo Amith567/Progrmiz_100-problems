d3b26bc9f6b781379220b762b75a4c81e1471700
Amith P <amithanilp@gmail.com>
2025-11-26 20:23:27 +0530

    Add Python program to print all prime numbers in a given interval

---
+++ 15.Python Program to Print all Prime Numbers in an Interval.py
@@ -0,0 +1,4 @@
+start=int(input("enter the number that you want to start : "))
+stop=int(input("enter the number that you want to stop : "))
+for num in range(start,stop+1):
+    print(f"{num}",end=",")
