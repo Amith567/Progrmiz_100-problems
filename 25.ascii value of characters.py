fe030840130458e79e8a55597a08f44a3c74b6f3
Amith P <amithanilp@gmail.com>
2025-11-28 11:12:30 +0530

    Add Python program to convert characters to ASCII values and vice versa

---
+++ 25.ascii value of characters.py
@@ -0,0 +1,4 @@
+char=input("enter the character : ")
+print(f'{char} equivalent ASCII value is {ord(char)}')
+num=int(input("enter a number : "))
+print(f'{num} is equivalent to {chr(num)} character')
