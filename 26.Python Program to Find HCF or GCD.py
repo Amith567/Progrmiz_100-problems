fb8f9690044c18c061649e1fcedabd5fcd9f83c3
Amith P <amithanilp@gmail.com>
2025-11-29 19:39:18 +0530

    Add Python program to find LCM

--- 26.Python Program to Find HCF or GCD.py
+++ 26.Python Program to Find HCF or GCD.py
@@ -1,9 +1,37 @@
 # Python Program to Find HCF or GCD
 
-num=int(input("enter a number : "))
-limit=int(num/2)+1
-hcf=[]
-for i in range (2,limit):
-    if num%i==0:
-        hcf.append(i)
-print(f"the highest common factor of {num} is {max(hcf)}" if hcf else f"{num} has no factors")
+num1=int(input('enter first number : '))
+num2=int(input('enter second number : '))
+arr1=list(filter(lambda a:num1%a==0,range(1,num1+1)))
+arr2=list(filter(lambda a:num2%a==0,range(1,num2+1)))
+
+arr1.sort(reverse=True)
+arr2.sort(reverse=True)
+
+hcf=None
+for x in arr2:
+    if x in arr1:
+        hcf=x
+        break
+
+print(f"the hcf is : {hcf}")
+
+
+
+
+# solution 2
+num1=int(input('enter first number : '))
+num2=int(input('enter second number : '))
+
+if num1>num2:
+    small=num2
+else:
+    small=num1
+
+for i in range(1,small+1):
+    if (num1%i==0)and (num2%i==0):
+        hcf=i
+print(f"the hcf is {hcf}")
+
+    
+        
---
+++ 27.Python Program to Find LCM.py
@@ -0,0 +1,12 @@
+num1=int(input('enter first number : '))
+num2=int(input('enter second number : '))
+
+if num1>num2:
+    small=num2
+else:
+    small=num1
+
+for i in range(1,small+1):
+    if (num1%i==0)and (num2%i==0):
+        hcf=i
+print(f"the hcf is {hcf}")
